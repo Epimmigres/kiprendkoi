2583398206de5bd8076bfbc0535cb57e3be5fc62
AriaK <arikassaei@gmail.com>
2021-05-05 04:08:09 +0200

    feat(repo-dp): Beginning of Repository DP
    - Created Dbo Classes
    - Created Automapper Profile
    - Created Repository Interface
    - Implemented Repository Interface

---
+++ WebApplication/DataAccess/AutomapperProfiles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.DataAccess
+{
+    public class AutomapperProfiles : AutoMapper.Profile
+    {
+        public AutomapperProfiles()
+        {
+            CreateMap<Dbo.Event, EfModels.Event>();
+            CreateMap<EfModels.Event, Dbo.Event>();
+            CreateMap<Dbo.Category, EfModels.Category>();
+            CreateMap<EfModels.Category, Dbo.Category>();
+            CreateMap<Dbo.Item, EfModels.Item>();
+            CreateMap<EfModels.Item, Dbo.Item>();
+        }
+    }
+}
---
+++ WebApplication/Dbo/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Dbo
+{
+    public class Item : IObjectWithId
+    {
+        public long Id { get; set; }
+        public string Who { get; set; }
+        public string What { get; set; }
+        public int Quantity { get; set; }
+        public long CategoryId { get; set; }
+    }
+}
